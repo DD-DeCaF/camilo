b1de5d4b0a1c8a33ec0ad8eebccdd2acd630cbb8
Henning Redestig <henning.red@gmail.com>
2017-04-03 10:21:35 +0200

    feat: add metabolica_client
    squash! feat: add metabolica_client

--- camilo/__init__.py
+++ camilo/__init__.py
@@ -1,6 +1,7 @@
 import logging
 import sys
 from camilo import settings
+from camilo.settings import metabolica_client
 from camilo.services import list_experiments, get_model, maps
 
 logger = logging.getLogger('camilo')
--- camilo/settings.py
+++ camilo/settings.py
@@ -1,5 +1,9 @@
 import os
 import configparser
+from functools import lru_cache
+from potion_client import Client
+from potion_client.auth import HTTPBearerAuth
+import requests
 
 RC_FILE = os.path.expanduser('~/.camilorc')
 config = configparser.ConfigParser()
@@ -18,3 +22,15 @@ class Default(object):
         'defaults', 'decaf_api', fallback=None)
     NOT_PUBLIC = {'NPC'}
     headers = {'Authorization': M8A_TOKEN} if M8A_TOKEN else {}
+
+
+@lru_cache(128)
+def metabolica_client():
+    """Get a client for metabolica.
+    """
+    requests.packages.urllib3.disable_warnings()
+    return Client(
+        Default.M8A_TOKEN,
+        auth=HTTPBearerAuth(Default.M8A_TOKEN),
+        verify=False
+    )
